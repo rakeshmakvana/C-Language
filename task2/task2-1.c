d401e2f9c1907b53184bbad70db97bd6ded0b5a1
rakeshmakvana <rakeshmakvana2108@gmail.com>
2023-07-26 13:46:07 +0530

    task2

---
+++ task2/task2-1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+main(){
+
+	//1
+	int a, b, c;
+	
+	printf("Enter A = ");
+	scanf("%d",&a);
+	
+	printf("Enter B = ");
+	scanf("%d",&b);
+	
+	printf("Enter C = ");
+	scanf("%d",&c);
+	
+	
+	if(a < b){
+		if(a < c){
+			printf("A is min...");
+		}
+		else{
+			printf("C is min...");
+		}
+	}
+	else{
+		if(b < c){
+			printf("B is min...");
+		}
+		else{
+			printf("C is min...");
+		}
+	}	
+
+
+}
---
+++ task2/task2-2.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+main(){
+	
+	//2
+	int a, b, c, d;
+	
+	printf("Enter A = ");
+	scanf("%d",&a);
+	
+	printf("Enter B = ");
+	scanf("%d",&b);
+	
+	printf("Enter C = ");
+	scanf("%d",&c);
+	
+	printf("Enter D = ");
+	scanf("%d",&d);
+	
+	
+	if(a > b){
+		if(a > c){
+			if(a > d){
+				printf("A is max...");
+			}
+			else{
+				printf("D is max...");
+			}
+		}
+		else{
+			if(c > d){
+				printf("C is max...");	
+			}
+			else{
+				printf("D is max...");	
+			}
+		}
+	}
+	else{
+		if(b > c){
+			if(b > d){
+				printf("B is max...");
+			}
+			else{
+				printf("D is max...");
+			}
+		}
+		else{
+			if(c > d){
+				printf("C is max...");	
+			}
+			else{
+				printf("D is max...");	
+			}
+		}
+	}
+
+	
+}
